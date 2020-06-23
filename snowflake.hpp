71c1342ec3641121a32dfbb5b65b50f49b01d76a
bruce <hanyongtao@live.com>
2020-06-23 11:18:56 +0800

    commit

--- snowflake.hpp
+++ snowflake.hpp
@@ -22,13 +22,13 @@ class snowflake
     static constexpr int64_t TWEPOCH = Twepoch;
     static constexpr int64_t WORKER_ID_BITS = 5L;
     static constexpr int64_t DATACENTER_ID_BITS = 5L;
-    static constexpr int64_t MAX_WORKER_ID = -1L ^ (-1L << WORKER_ID_BITS);
-    static constexpr int64_t MAX_DATACENTER_ID = -1L ^ (-1L << DATACENTER_ID_BITS);
+    static constexpr int64_t MAX_WORKER_ID = (1 << WORKER_ID_BITS) - 1;
+    static constexpr int64_t MAX_DATACENTER_ID = (1 << DATACENTER_ID_BITS) - 1;
     static constexpr int64_t SEQUENCE_BITS = 12L;
     static constexpr int64_t WORKER_ID_SHIFT = SEQUENCE_BITS;
     static constexpr int64_t DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
     static constexpr int64_t TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS;
-    static constexpr int64_t SEQUENCE_MASK = -1L ^ (-1L << SEQUENCE_BITS);
+    static constexpr int64_t SEQUENCE_MASK = (1 << SEQUENCE_BITS) - 1;
 
     using time_point = std::chrono::time_point<std::chrono::steady_clock>;
 
